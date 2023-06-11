2c9e66e837f70ffa9c1de69e988ed7debb0afb20
JehanKandy <jehankandy@gmail.com>
2023-06-11 17:37:23 +0530

    Update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->integer('role'); //Admin - 1, User - 2
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
