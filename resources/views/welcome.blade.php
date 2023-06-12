ca0bec026fafbd38cb738972328e5f03f594a0ef
JehanKandy <jehankandy@gmail.com>
2023-06-12 20:44:37 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,15 +21,10 @@
         <div class="main-content">
             <div class="btn-group me-2" role="group" aria-label="First group">
                 <button type="button" class="btn btn-primary">1</button>
-                <button type="button" class="btn btn-primary">2</button>
-                <button type="button" class="btn btn-primary">3</button>
-                <button type="button" class="btn btn-primary">4</button>
               </div>
               <div class="btn-group me-2" role="group" aria-label="Second group">
                 <button type="button" class="btn btn-secondary">5</button>
-                <button type="button" class="btn btn-secondary">6</button>
-                <button type="button" class="btn btn-secondary">7</button>
-              </div>
+            </div>
         </div>
 
     </body>
