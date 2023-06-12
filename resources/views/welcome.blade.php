b4d1c6babdfdbd23d8432647d5a7bee133b9db47
JehanKandy <jehankandy@gmail.com>
2023-06-12 20:56:10 +0530

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,11 +20,17 @@
 
         <div class="main-content">
             <div class="btn-main">
-                <div class="btn-group me-2" role="group" aria-label="First group">
-                    <button class="login-btn">Login</button>
-                </div>
-                <div class="btn-group me-2" role="group" aria-label="Second group">
-                    <button class="reg-btn">Register</button>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="btn-group me-2" role="group" aria-label="First group">
+                            <button class="login-btn">Login</button>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="btn-group me-2" role="group" aria-label="Second group">
+                            <button class="reg-btn">Register</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
