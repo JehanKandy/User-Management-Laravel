eab276b80a88bc91187b8e6dbed9cac10c15f35d
JehanKandy <jehankandy@gmail.com>
2023-06-12 19:21:29 +0530

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\Admin::class,
+        'staff' => \App\Http\Middleware\Staff::class,
+        'client' => \App\Http\Middleware\Client::class,
     ];
 }
