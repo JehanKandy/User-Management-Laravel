618e2aa52585a13602254fbc04345170e927c1ec
JehanKandy <jehankandy@gmail.com>
2023-06-13 21:12:54 +0530

    Update

--- resources/views/layouts/admin_dash.blade.php
+++ resources/views/layouts/admin_dash.blade.php
@@ -136,7 +136,7 @@
                             <li><a class="dropdown-item" href="#">Settings</a></li>
                             <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                 document.getElementById('logout-form').submit();">
-                   {{ __('Logout') }}>Logout</a>
+                   {{ __('Logout') }}</a>
                 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                     @csrf
