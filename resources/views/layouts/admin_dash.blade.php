6231570cda768d8b40fbf9b75878ada67e17b87b
JehanKandy <jehankandy@gmail.com>
2023-06-13 19:28:27 +0530

    Update

--- resources/views/layouts/admin_dash.blade.php
+++ resources/views/layouts/admin_dash.blade.php
@@ -35,4 +35,22 @@
             </a>
         </div>
     </div>
+
+    {{-- Side bar end --}}
+
+    <div class="page-content-wrapper">
+        <nav class="navbar navbar-expand-lg navbar-light bg-transparent py4 px-4">
+            <div class="d-flex align-items-center">
+                <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle">
+                    <h2 class="fs-2 m-0">Dashboard</h2>
+                </i>
+            </div>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </nav>
+    </div>
 </div>
