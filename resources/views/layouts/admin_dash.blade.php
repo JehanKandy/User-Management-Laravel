2c61e69e9e4d92ca4ec5b10cbe11a5221833e4e7
JehanKandy <jehankandy@gmail.com>
2023-06-13 19:11:35 +0530

    Update

--- resources/views/layouts/admin_dash.blade.php
+++ resources/views/layouts/admin_dash.blade.php
@@ -5,5 +5,10 @@
         <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase">
             <i class="fas fa-tachometer-alt me-2"></i> User Management
         </div>
+        <div class="list-group list-group-flush my-3">
+            <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active">
+                <i class="fas fa-tachometer-alt me-2"></i> Dashboard
+            </a>
+        </div>
     </div>
 </div>
