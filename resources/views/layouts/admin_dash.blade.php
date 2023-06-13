50a5a3d5ec01c088e3e8602946fe15aee5e59cc5
JehanKandy <jehankandy@gmail.com>
2023-06-13 19:08:33 +0530

    Update

--- resources/views/layouts/admin_dash.blade.php
+++ resources/views/layouts/admin_dash.blade.php
@@ -3,7 +3,7 @@
 <div class="d-flex" id="wrapper">
     <div class="bg-white">
         <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase">
-            <i class="fas fa-tachometer-alt me-2"></i> Dashboard
+            <i class="fas fa-tachometer-alt me-2"></i> User Management
         </div>
     </div>
 </div>
