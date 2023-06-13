8e8a82d14c55aced3fee3696716cde85a45015cd
JehanKandy <jehankandy@gmail.com>
2023-06-13 19:16:46 +0530

    Update

--- resources/views/layouts/admin_dash.blade.php
+++ resources/views/layouts/admin_dash.blade.php
@@ -9,7 +9,7 @@
             <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active">
                 <i class="fas fa-tachometer-alt me-2"></i> Dashboard
             </a>
-            <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active">
+            <a href="#" class="list-group-item list-group-item-action bg-transparent second-text fw-bold">
                 <i class="fas fa-project-diagram me-2"></i> Projects
             </a>
             <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active">
@@ -19,13 +19,13 @@
                 <i class="fas fa-paperclip-alt me-2"></i> Report
             </a>
             <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active">
-                <i class="fas fa-tachometer-alt me-2"></i> Dashboard
+                <i class="fas fa-shopping-cart me-2"></i> Store Mng
             </a>
             <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active">
-                <i class="fas fa-tachometer-alt me-2"></i> Dashboard
+                <i class="fas fa-gift me-2"></i> Products
             </a>
             <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active">
-                <i class="fas fa-tachometer-alt me-2"></i> Dashboard
+                <i class="fas fa-comment-dot me-2"></i> Chat
             </a>
             <a href="#" class="list-group-item list-group-item-action bg-transparent second-text active">
                 <i class="fas fa-tachometer-alt me-2"></i> Dashboard
