9acd3681ca6415eee5f5a95095eb86f0294d569b
JehanKandy <jehankandy@gmail.com>
2023-06-13 19:01:03 +0530

    Update

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -1,7 +1,8 @@
-@extends('layouts.app')
+@extends('layouts.admin_dash')
+
 
 @section('content')
-<div class="container">
+{{-- <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -19,5 +20,5 @@
             </div>
         </div>
     </div>
-</div>
+</div> --}}
 @endsection
