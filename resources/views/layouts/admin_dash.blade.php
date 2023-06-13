ba1acb965476d8f7f2be9aaf69f1ecb261f79cfe
JehanKandy <jehankandy@gmail.com>
2023-06-13 20:03:42 +0530

    Update

--- resources/views/layouts/admin_dash.blade.php
+++ resources/views/layouts/admin_dash.blade.php
@@ -1,8 +1,8 @@
 @extends('header')
 
 <div class="d-flex" id="wrapper">
-    <div class="bg-white">
-        <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase">
+    <div class="bg-white" id="sidebar-wrapper">
+        <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase border-bottom">
             <i class="fas fa-tachometer-alt me-2"></i> User Management
         </div>
         <div class="list-group list-group-flush my-3">
