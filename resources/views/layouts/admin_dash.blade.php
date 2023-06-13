bdd1c1e22aa90e5a6fd1eccdac46f8d489f3d412
JehanKandy <jehankandy@gmail.com>
2023-06-13 18:56:17 +0530

    Update

--- resources/views/layouts/admin_dash.blade.php
+++ resources/views/layouts/admin_dash.blade.php
@@ -2,6 +2,8 @@
 
 <div class="d-flex" id="wrapper">
     <div class="bg-white">
-        <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase"></div>
+        <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase">
+            <i class="fas fa-user-secret me-2"></i> Dashboard
+        </div>
     </div>
 </div>
