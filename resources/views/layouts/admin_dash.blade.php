7f436cc0210105a97fe6a8abea6ca309a9836681
JehanKandy <jehankandy@gmail.com>
2023-06-13 18:55:34 +0530

    Update

--- resources/views/layouts/admin_dash.blade.php
+++ resources/views/layouts/admin_dash.blade.php
@@ -1,1 +1,7 @@
 @extends('header')
+
+<div class="d-flex" id="wrapper">
+    <div class="bg-white">
+        <div class="sidebar-heading text-center py-4 primary-text fs-4 fw-bold text-uppercase"></div>
+    </div>
+</div>
