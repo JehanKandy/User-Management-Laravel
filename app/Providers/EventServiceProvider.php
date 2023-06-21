55a02837f09d7372925c5a7a01999263a62c2889
JehanKandy <jehankandy@gmail.com>
2023-06-21 19:41:08 +0530

    Update

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Event;
 
-event(new Registered($user));
+
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -19,6 +19,7 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+            LogVerifiedUser::class,
         ],
     ];
 
