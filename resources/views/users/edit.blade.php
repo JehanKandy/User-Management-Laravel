f392cfddccfe1f81c0fc48725789f4bd095711d5
JehanKandy <jehankandy@gmail.com>
2023-06-24 20:21:24 +0530

    Update

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -6,7 +6,11 @@
         <form action="{{ url('users/' .$members->id) }}" method="POST">
             <h3>Name : <b> {{ $members->name }} </b></h3><br><br>
             <h5>Email : {{ $members->email }}</h5>
-            
+
+            <input type="radio" name="role" value="1">Admin <br>
+            <input type="radio" name="role" value="2">Staff <br>
+            <input type="radio" name="role" value="3">Client <br>
+
         </form>
     </div>
 
