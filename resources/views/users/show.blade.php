854884c35e6556f5c9deca39c86d4db121849365
JehanKandy <jehankandy@gmail.com>
2023-06-24 20:09:43 +0530

    Update

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,19 +1,8 @@
-@extends('students.layout')
+@extends('layouts.admin_dash')
+
+
 @section('content')
- 
- 
-<div class="card">
-  <div class="card-header">Students Page</div>
-  <div class="card-body">
-   
- 
-        <div class="card-body">
-        <h5 class="card-title">Name : {{ $members->name }}</h5>
-        <p class="card-text">Address : {{ $members->email }}</p>
-        <p class="card-text">Mobile : {{ $members->role }}</p>
-  </div>
-       
-    </hr>
-  
-  </div>
-</div>
+    <div class="card-admin">
+        <h3>Name : <b> {{ $members->name }} </b></h3>
+    </div>
+@endsection
