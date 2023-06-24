663bafd0271ab5887ae2fc80f5406d27eb23cb0b
JehanKandy <jehankandy@gmail.com>
2023-06-24 19:43:50 +0530

    Update

--- routes/web.php
+++ routes/web.php
@@ -20,9 +20,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes([
-    'verify' => true
-]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
