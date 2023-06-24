92cb2e1a9f6e14eb0eaa23aa5ad9ac94b4bb932e
JehanKandy <jehankandy@gmail.com>
2023-06-24 19:15:32 +0530

    Update

--- resources/views/users/all_user.blade.php
+++ resources/views/users/all_user.blade.php
@@ -6,11 +6,17 @@
         <table class="table">
             <thead class="table-primary">
                 <tr>
-                    <th>
-                        Name
-                    </th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Action</th>
                 </tr>
             </thead>
+            <tbody>
+                @foreach ($collection as $item)
+                    
+                @endforeach
+            </tbody>
         </table>
     </div>
 
