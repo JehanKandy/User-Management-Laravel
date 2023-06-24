50ed90bb3e3335bea70387863e711cd644722cf6
JehanKandy <jehankandy@gmail.com>
2023-06-24 20:18:23 +0530

    Update

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -4,6 +4,8 @@
 
     <div class="admin-card">
         <form action="{{ url('users/' .$members->id) }}" method="POST">
+            <h3>Name : <b> {{ $members->name }} </b></h3><br><br>
+            <h5>Email : {{ $members->email }}</h5>
             
         </form>
     </div>
