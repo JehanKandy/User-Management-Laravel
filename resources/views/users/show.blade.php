ecdfbb5c53a9fdc0f90a0bf4d1b5bc64a22e1ae6
JehanKandy <jehankandy@gmail.com>
2023-06-24 20:12:14 +0530

    Update

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -5,5 +5,22 @@
     <div class="admin-card">
         <h3>Name : <b> {{ $members->name }} </b></h3><br><br>
         <h5>Email : {{ $members->email }}</h5>
+        <h5>
+            @php
+                switch ($members->role) {
+                    case 1:
+                        echo "<h4><span class='badge bg-danger'>Admin</span></h4>"
+                        break;
+
+                    case 2:
+                        echo "<h4><span class='badge bg-danger'>Admin</span></h4>"
+                        break;
+
+                    case 3:
+                        echo "<h4><span class='badge bg-danger'>Admin</span></h4>"
+                        break;
+                }
+            @endphp
+        </h5>
     </div>
 @endsection
