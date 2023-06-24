d293603906bf692a2b8598ebd6a88b5cecfea17d
JehanKandy <jehankandy@gmail.com>
2023-06-24 20:34:59 +0530

    Update

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -40,7 +40,15 @@
                     </td>
                     <td>
                         <a href="{{ url('/users/' . $item->id) }}" title="View Student"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                        <a href="{{ url('/users/' . $item->id . '/edit') }}" title="Edit Student"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                        @php
+                            if($item->role == 1){
+                                echo "Admin";
+                            }
+                            else{
+                                echo "<a href='{{ url('/users/' . $item->id . '/edit') }}' title='Edit Student'><button class='btn btn-primary btn-sm'><i class='fa fa-pencil-square-o' aria-hidden='true'></i> Edit</button></a>"
+                            }
+                        @endphp
+                        
                     </td>
                 </tr>
             @endforeach
