dd49ded9a2a37468434de9903b42761229745419
JehanKandy <jehankandy@gmail.com>
2023-06-24 20:31:05 +0530

    Update

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,6 +8,24 @@
             @method("PATCH")
             <h3>Name : <b> {{ $members->name }} </b></h3><br><br>
             <h5>Email : {{ $members->email }}</h5>
+            <h5>User Type : 
+                @php
+                    switch ($members->role) {
+                        case 1:
+                            echo "<h4><span class='badge bg-danger'>Admin</span></h4>";
+                            break;
+                        case 2:
+                            echo "<h4><span class='badge bg-warning'>Staff</span></h4>";
+                            break;
+                        case 3:
+                            echo "<h4><span class='badge bg-success'>Client</span></h4>";
+                            break;
+                        
+                        default:                                    
+                            break;
+                    }
+                @endphp
+            </h5>
 
             <input type="radio" name="role" value="1">Admin <br>
             <input type="radio" name="role" value="2">Staff <br>
