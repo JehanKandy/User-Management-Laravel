9bf809ed155fa9a2021ff266c0ec76030fc401a8
JehanKandy <jehankandy@gmail.com>
2023-06-24 20:05:28 +0530

    Update

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,9 +8,9 @@
    
  
         <div class="card-body">
-        <h5 class="card-title">Name : {{ $students->name }}</h5>
-        <p class="card-text">Address : {{ $students->address }}</p>
-        <p class="card-text">Mobile : {{ $students->mobile }}</p>
+        <h5 class="card-title">Name : {{ $members->name }}</h5>
+        <p class="card-text">Address : {{ $members->email }}</p>
+        <p class="card-text">Mobile : {{ $members->role }}</p>
   </div>
        
     </hr>
