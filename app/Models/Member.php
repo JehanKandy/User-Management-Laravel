309390b76bb53fa859e207d605f1805ab92c15f1
JehanKandy <jehankandy@gmail.com>
2023-06-24 19:24:11 +0530

    Update

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Member extends Model
+class Student extends Model
 {
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'email', 'role'];
     use HasFactory;
 }
