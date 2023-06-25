387e39b1af13cd25c1a1fecc8b48fcbc1078c7d8
JehanKandy <jehankandy@gmail.com>
2023-06-25 06:50:19 +0530

    Update

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -41,8 +41,19 @@
                     <td>
                         <a href="{{ url('/users/' . $item->id) }}" title="View Student"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
 
-                        
+                        @php
+                            if(auth()->user()->role == 2 || auth()->user()->role == 3){
+                                echo "Only Show";
+                            }
+                            else{
+
+                        @endphp
+
                         <a href="{{ url('/users/' . $item->id . '/edit') }}" title="Edit Student"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                                
+                        @php
+                            }
+                        @endphp
                     </td>
                 </tr>
             @endforeach
